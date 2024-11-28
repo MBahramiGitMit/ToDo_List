bdd5ab6eb08bf46e2c38a4143580ebe77182039b
MehdiBahrami <mbahrami900913@gmail.com>
2024-11-28 08:56:06 +0330

    Setup ROOM Database

--- app/src/main/java/com/mbahrami/todolist/data/models/ToDoTask.kt
+++ app/src/main/java/com/mbahrami/todolist/data/models/ToDoTask.kt
@@ -1,8 +1,14 @@
 package com.mbahrami.todolist.data.models
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.mbahrami.todolist.util.Constants
+
+@Entity(tableName = Constants.DATABASE_TABLE)
 data class ToDoTask(
-    val id:Int,
-    val title:String,
-    val description:String,
-    val priority:Priority
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val title: String,
+    val description: String,
+    val priority: Priority
 )
